69061a5b945de267144800976cf3292ffee026fe
Ali Abooof <aliabooof@outlook.com>
2025-01-07 14:19:25 +0200

    Adding department model class in dal layer

---
+++ DEMO_PL/Demo.DAL/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Code is required!!")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is required!!")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime DateOfCreation { get; set; }
+    }
+}
