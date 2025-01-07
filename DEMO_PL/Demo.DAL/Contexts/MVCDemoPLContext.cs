163a89986cd7117b30fd1378507bb211abca293e
Ali Abooof <aliabooof@outlook.com>
2025-01-07 14:29:50 +0200

    Creating DB context

---
+++ DEMO_PL/Demo.DAL/Contexts/MVCDemoPLContext.cs
@@ -0,0 +1,17 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Contexts
+{
+    internal class MVCDemoPLContext : DbContext 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("server = .; Database = MVCDempPL; Trusted_connection = true; TrustServeCertificate = true "); //MultipleActiveResultSets = true; 
+       public DbSet<Department> departmen { get; set; }
+    }
+}
